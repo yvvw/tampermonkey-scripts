74d3c8d93dba62104e474fdcfae4d4347bdac4cc
yvvw <g592842897@gmail.com>
2026-08-09 22:30:48 +0800

    douyin

--- src/douyin.user.ts
+++ src/douyin.user.ts
@@ -2,8 +2,8 @@
 // @name         Better Douyin
 // @namespace    https://github.com/yvvw/browser-scripts
 // @homepageURL  https://github.com/yvvw/browser-scripts/blob/main/src/douyin.user.ts
-// @version      0.0.3
-// @description  自动网页全屏、关闭礼物特效
+// @version      0.0.4
+// @description  网页全屏、隐藏礼物、切换画质
 // @author       yvvw
 // @icon         https://www.douyin.com/favicon.ico
 // @license      MIT
@@ -18,26 +18,47 @@ import { HTMLUtils, Logger } from './util'
 const logger = Logger.new('Better Douyin')
 
 window.onload = function main() {
-  hideGift()
+  blockGift()
   switchWebFullscreen()
+  switchHighestQuality()
+  hideElements()
 }
 
-function hideGift() {
-  HTMLUtils.query(
-    () =>
-      HTMLUtils.getFirstElementByXPath<HTMLDivElement>('//div[text()="屏蔽礼物特效"]')
-        ?.nextElementSibling as HTMLElement
-  )
-    .then((el) => el.click())
+function hideElements() {
+  const style = document.createElement('style')
+  style.textContent =
+    ['#BottomLayout', '#ShortTouchLayout', '#room_info_bar'].join(', ') + ' { display: none !important; }'
+  document.head.appendChild(style)
+}
+
+function blockGift() {
+  HTMLUtils.query(() => document.querySelector<HTMLElement>('[data-e2e="gift-setting"]') ?? null)
+    .then((giftSetting) => {
+      giftSetting.dispatchEvent(new MouseEvent('mouseover', { bubbles: true }))
+      return HTMLUtils.query(
+        () =>
+          HTMLUtils.getFirstElementByXPath<HTMLElement>(
+            '//span[text()="屏蔽礼物特效"]/following-sibling::*[@data-e2e="effect-switch"]'
+          ) ?? null
+      ).then((el) => ({ giftSetting, el }))
+    })
+    .then(({ giftSetting, el }) => {
+      ;(el.firstElementChild as HTMLElement).click()
+      giftSetting.dispatchEvent(new MouseEvent('mouseout', { bubbles: true }))
+    })
     .catch((err) => logger.error('hideGift', err))
 }
 
 function switchWebFullscreen() {
-  HTMLUtils.query(
-    () =>
-      HTMLUtils.getFirstElementByXPath<HTMLSpanElement>('//span[text()="网页全屏"]')?.parentElement?.parentElement
-        ?.nextElementSibling as HTMLDivElement
-  )
+  HTMLUtils.query(() => document.querySelector<HTMLElement>('.chatroom_close') ?? null)
     .then((el) => el.click())
     .catch((err) => logger.error('switchWebFullscreen', err))
+}
+
+function switchHighestQuality() {
+  HTMLUtils.query(
+    () => (document.querySelector('[data-e2e="quality-selector"]')?.firstElementChild as HTMLElement) ?? null
+  )
+    .then((el) => el.click())
+    .catch((err) => logger.error('switchHighestQuality', err))
 }
